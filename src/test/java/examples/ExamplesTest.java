97dc006381d40a368728f5a47b5491f1f2a0faeb
meridicolos <pavel.matveev2@gmail.com>
2022-03-30 18:35:46 +0300

    update

--- src/test/java/examples/ExamplesTest.java
+++ src/test/java/examples/ExamplesTest.java
@@ -1,14 +1,31 @@
 package examples;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 
 class ExamplesTest {
 
-    // this will run all *.feature files that exist in sub-directories
-    // see https://github.com/intuit/karate#naming-conventions
-    @Karate.Test
-    Karate testAll() {
-        return Karate.run().relativeTo(getClass());
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:examples/users").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        generateReport(results.getReportDir());
     }
-
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "KarateTest");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
